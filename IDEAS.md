ad6911a575f86fb6f481ccb055961f50d42f976e
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2023-04-01 21:56:50 +0200

    Dialogue! 💬

---
+++ IDEAS.md
@@ -0,0 +1,50 @@
+# bevy jam 2023
+
+tema: **side effects**
+
+### ideas
+
+++ conversación en la que puedes decir cosas pero al hacerlo puedes causarle
+triggers a la persona con la que estás hablando
+
++ construír cosas y si no haces bien la base se te cae el resto
+
+- juego puzzles cuántics
+
+-- pastillas, beat em up, cosas buenas y malas
+
++ hot girls have tummy issues
+
+---
+
+lo reducimos a:
+
+**juego conversación**
+
+opciones predeterminadas o libre?
+pueden ser opciones predeterminadas pero cada una con huecos, cards against humanity
+puedes tener un mazo de conversación
+el escenario puede ser un bar
+
+**juego construír**
+
+pastillas, comida, etc... van cayendo
+tipo tetris o tu eliges cuando soltarlas?
+
+---
+
+bar de santiago, luz tenue
+poder mirar alrededor estaría guay pero no es super prioritario
+estilo artístico: me suena bien pixel art 3d pero plano y en billboards,
+que sea cuqui pero tampoco tanto, adaptativo con la ansiedad
+
+escribir dialogos herramienta
+sonido herramienta
+
+multijugador donde dices lo que gusta o no
+puede ser postironico tipo ratea lo que te gustó con caritas felices
+les podemos dar un tema de conversación
+
+no es muy factible porque no hay mucha gente jugando
+se puede dejar como un objetivo futuro porque si diseñamos bien el juego
+este tipo de multijugador no debería de ser imposible de hacer
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
+use std::io::BufRead;
+
 use bevy::prelude::*;
+use yarn_spool::{read_string_table_file, expand_substitutions, Dialogue, DialogueEvent, Program as DialogueProgram};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, init)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
         texture: asset_server.load("icon.png"),
         ..Default::default()
     });
+}
+
+fn dialogue() {
+    let dialogue_program = DialogueProgram::from_file("assets/dialogue/build/test.yarnc");
+    let dialogue_lines = read_string_table_file("assets/dialogue/build/test-Lines.csv");
+
+    let mut dialogue = Dialogue::new();
+    dialogue.add_program(&dialogue_program);
+
+    let mut input = String::new();
+
+    while let Some(e) = dialogue.advance() {
+        match e {
+            DialogueEvent::Line => {
+                let line = dialogue.current_line();
+                let raw_text = &dialogue_lines[&line.id].text;
+                let text = expand_substitutions(raw_text, &line.substitutions);
+                println!("{}", text);
+            },
+            DialogueEvent::Command => {
+                println!("<<{}>>", dialogue.current_command());
+            },
+            DialogueEvent::Options => {
+                for opt in dialogue.current_options() {
+                    let raw_text = &dialogue_lines[&opt.line.id].text;
+                    let text = expand_substitutions(raw_text, &opt.line.substitutions);
+                    println!("{}) {}", opt.index, text);
+                }
+
+                input.clear();
+                std::io::stdin().lock().read_line(&mut input).expect("can't read input");
+                let option = input.trim().parse().expect("can't parse input");
+                dialogue.set_selected_option(option);
+            }
+        }
+    }
 }
