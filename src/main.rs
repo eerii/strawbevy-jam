08bfb8c493875321015b09c564c0f90147155f79
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2023-04-01 14:53:28 +0200

    Update to Bevy Dev 🌵

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// Bevy code commonly triggers these lints and they may be important signals
-// about code quality. They are sometimes hard to avoid though, and the CI
-// workflow treats them as errors, so this allows them throughout the project.
-// Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::prelude::*;
@@ -9,7 +5,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+        .add_systems(Startup, setup)
         .run();
 }
 
